86beae42e6ea3ab4585932b960ac239a873d75af
zarik <fff@dd.dd>
2015-07-04 09:04:27 +0300

    hh

--- main/static/js/app.js
+++ main/static/js/app.js
@@ -3,9 +3,12 @@
         'restangular'
     ])
 
-    app.config(function ($stateProvider, $urlRouterProvider, RestangularProvider) {
+    app.config(function ($stateProvider, $routeProvider, $urlRouterProvider,  RestangularProvider) {
         // For any unmatched url, send to /route1
         $urlRouterProvider.otherwise("/");
+
+     
+
         $stateProvider
             .state('index', {
 
@@ -14,14 +17,38 @@
                 controller: function($scope, Restangular) {
                     $scope.items = ["A", "List", "Of", "Items"];
                     var baseAccounts = Restangular.all('accounts');
-                    baseAccounts.getList().then(function(accounts) {
-                      $scope.allAccounts = accounts;
-                    });
+                    //baseAccounts.getList().then(function(accounts) {
+                    //  $scope.allAccounts = accounts;
+                    //});
                   }
             })
+            .state('register', {
+
+                url: "/register",
+                templateUrl: "/static/templates/_registration.html",
+                controller: function($scope, Restangular) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                    var baseAccounts = Restangular.all('accounts');
+                    //baseAccounts.getList().then(function(accounts) {
+                    //  $scope.allAccounts = accounts;
+                    //});
+                  }
+            });
+
+
+        
 
           
     })
+    .controller('RegistrationController', ['$scope', function($scope) {
+      $scope.list = [];
+      $scope.text = 'hello';
+      $scope.submit = function() {
+        
+          alert($scope.email) ;
+        
+      };
+    }]);
 
    // app.controller("IndexController", ['$scope', 'Restangular', 'CbgenRestangular', '$q',
    // function ($scope, Restangular, CbgenRestangular, $q) {
